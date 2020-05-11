591386d2d74bbb6f3d6bd9e2d89b3eee55bcb966
Florian Walter <fw@digio.ch>
2020-05-11 13:24:54 +0200

    added key pair api service

--- pkg/flow/keypair.go
+++ pkg/flow/keypair.go
@@ -1,7 +1,84 @@
 package flow
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"path"
+)
+
+type KeyPairService interface {
+	List(ctx context.Context, options PaginationOptions) ([]*KeyPair, *Response, error)
+	Create(ctx context.Context, data *KeyPairCreate) (*KeyPair, *Response, error)
+	Delete(ctx context.Context, id Id) (*Response, error)
+}
+
 type KeyPair struct {
 	Id          Id     `json:"id"`
 	Name        string `json:"name"`
 	Fingerprint string `json:"fingerprint"`
+}
+
+func (k *KeyPair) String() string {
+	return k.Name
+}
+
+type KeyPairCreate struct {
+	Name      string `json:"name"`
+	PublicKey string `json:"public_key"`
+}
+
+type keyPairService struct {
+	client *Client
+}
+
+func (s *keyPairService) List(ctx context.Context, options PaginationOptions) ([]*KeyPair, *Response, error) {
+	p := path.Join("/v3/", s.client.OrganizationPath(), "compute/key-pairs")
+	p, err := addOptions(p, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, p, nil, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var val []*KeyPair
+
+	res, err := s.client.Do(req, &val)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return val, res, nil
+}
+
+func (s *keyPairService) Create(ctx context.Context, data *KeyPairCreate) (*KeyPair, *Response, error) {
+	p := path.Join("/v3/", s.client.OrganizationPath(), "compute/key-pairs")
+
+	req, err := s.client.NewRequest(ctx, http.MethodPost, p, data, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	val := &KeyPair{}
+
+	res, err := s.client.Do(req, val)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return val, res, nil
+}
+
+func (s *keyPairService) Delete(ctx context.Context, id Id) (*Response, error) {
+	p := path.Join("/v3/", s.client.OrganizationPath(), fmt.Sprintf("compute/key-pairs/%d", id))
+
+	req, err := s.client.NewRequest(ctx, http.MethodDelete, p, nil, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(req, nil)
 }
