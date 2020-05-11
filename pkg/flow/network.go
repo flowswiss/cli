fbf4a792529c30bbee87f0c5cfff9a826af5d2b3
Florian Walter <fw@digio.ch>
2020-05-11 13:25:25 +0200

    added network api service

--- pkg/flow/network.go
+++ pkg/flow/network.go
@@ -1,10 +1,85 @@
 package flow
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"path"
+)
+
+type NetworkService interface {
+	List(ctx context.Context, options PaginationOptions) ([]*Network, *Response, error)
+	Get(ctx context.Context, id Id) (*Network, *Response, error)
+	Create(ctx context.Context, data *KeyPairCreate) (*Network, *Response, error)
+	Delete(ctx context.Context, id Id) (*Response, error)
+}
+
 type Network struct {
-	Id       Id       `json:"id"`
-	Name     string   `json:"name"`
-	Cidr     string   `json:"cidr"`
-	Location Location `json:"location"`
-	UsedIps  int      `json:"used_ips"`
-	TotalIps int      `json:"total_ips"`
+	Id                  Id       `json:"id"`
+	Name                string   `json:"name"`
+	Description         string   `json:"description"`
+	Cidr                string   `json:"cidr"`
+	Location            Location `json:"location"`
+	DomainNameServers   []string `json:"domain_name_servers"`
+	AllocationPoolStart string   `json:"allocation_pool_start"`
+	AllocationPoolEnd   string   `json:"allocation_pool_end"`
+	GatewayIp           string   `json:"gateway_ip"`
+	UsedIps             int      `json:"used_ips"`
+	TotalIps            int      `json:"total_ips"`
+}
+
+func (n *Network) String() string {
+	return n.Name
+}
+
+type networkService struct {
+	client *Client
+}
+
+func (s *networkService) List(ctx context.Context, options PaginationOptions) ([]*Network, *Response, error) {
+	p := path.Join("/v3/", s.client.OrganizationPath(), "compute/networks")
+	p, err := addOptions(p, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, p, nil, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var val []*Network
+
+	res, err := s.client.Do(req, &val)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return val, res, nil
+}
+
+func (s *networkService) Get(ctx context.Context, id Id) (*Network, *Response, error) {
+	p := path.Join("/v3/", s.client.OrganizationPath(), fmt.Sprintf("compute/networks/%d", id))
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, p, nil, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	val := &Network{}
+
+	res, err := s.client.Do(req, val)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return val, res, nil
+}
+
+func (s *networkService) Create(ctx context.Context, data *KeyPairCreate) (*Network, *Response, error) {
+	return nil, nil, nil
+}
+
+func (s *networkService) Delete(ctx context.Context, id Id) (*Response, error) {
+	return nil, nil
 }
---
+++ pkg/flow/network_test.go
@@ -0,0 +1,74 @@
+package flow
+
+import (
+	"context"
+	"net/http"
+	"path"
+	"testing"
+)
+
+func TestNetworkService_List(t *testing.T) {
+	setupMockServer(t)
+
+	serveMux.HandleFunc(path.Join("/v3/", client.OrganizationPath(), "/compute/networks"), func(res http.ResponseWriter, req *http.Request) {
+		response := `[{"id":1,"name":"Default Network","cidr":"172.31.0.0/20","location":{"id":1,"name":"ALP1"},"used_ips":6,"total_ips":3995}]`
+
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(200)
+		_, err := res.Write([]byte(response))
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	networks, _, err := client.Network.List(context.Background(), PaginationOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(networks) != 1 {
+		t.Fatal("expected amount of key pairs to be 1, got", len(networks))
+	}
+
+	network := networks[0]
+
+	if network.Id != 1 || network.Name != "Default Network" || network.Cidr != "172.31.0.0/20" {
+		t.Error("error while parsing network")
+	}
+
+	if network.Location.Id != 1 || network.Location.Name != "ALP1" {
+		t.Error("error while parsing location")
+	}
+}
+
+func TestNetworkService_Get(t *testing.T) {
+	setupMockServer(t)
+
+	serveMux.HandleFunc(path.Join("/v3/", client.OrganizationPath(), "/compute/networks/1"), func(res http.ResponseWriter, req *http.Request) {
+		response := `{"id":1,"name":"Default Network","description":"Initially created network","cidr":"172.31.0.0\/20","location":{"id":1,"name":"ALP1"},"domain_name_servers":["1.1.1.1","8.8.8.8"],"allocation_pool_start":"172.31.0.100","allocation_pool_end":"172.31.15.254","gateway_ip":"172.31.0.1","used_ips":6,"total_ips":3995}`
+
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(200)
+		_, err := res.Write([]byte(response))
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	network, _, err := client.Network.Get(context.Background(), 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if network.Id != 1 || network.Name != "Default Network" || network.Cidr != "172.31.0.0/20" {
+		t.Error("error while parsing network")
+	}
+
+	if network.AllocationPoolStart != "172.31.0.100" || network.AllocationPoolEnd != "172.31.15.254" {
+		t.Error("error while parsing allocation pool")
+	}
+
+	if len(network.DomainNameServers) != 2 {
+		t.Error("error while parsing domain name server")
+	}
+}
