4921685f4efd4e2568f775e8104075222fd4ff58
Florian Walter <nairolf.retlaw@gmail.com>
2022-07-01 13:52:11 +0200

    improved filter matching for selecting single item
    added matching type which determines how good the term matches with a
    given item. this allows to use filtered values unambiguously when only
    a single one has an exact match with the search term

--- pkg/filter/filter.go
+++ pkg/filter/filter.go
@@ -14,12 +14,8 @@ func Find[T Filterable](items []T, term string) []T {
 
 	var filtered []T
 	for _, item := range items {
-		identifiers := item.Keys()
-		for _, identifier := range identifiers {
-			if strings.Contains(strings.ToLower(identifier), term) {
-				filtered = append(filtered, item)
-				break
-			}
+		if matches(item, term) != noMatch {
+			filtered = append(filtered, item)
 		}
 	}
 
@@ -34,8 +30,59 @@ func FindOne[T Filterable](items []T, term string) (res T, err error) {
 	}
 
 	if len(filtered) > 1 {
-		return res, fmt.Errorf("term %q resulted in multiple results", term)
+		var bestMatch T
+		ambiguous := true
+
+		// find the best match (the one that matches exactly the term)
+		for _, item := range filtered {
+			if matches(item, term) == exactMatch {
+				if ambiguous {
+					bestMatch = item
+					ambiguous = false
+				} else {
+					// multiple matches found, return an error
+					return res, ambiguousError(term)
+				}
+			}
+		}
+
+		if ambiguous {
+			return res, ambiguousError(term)
+		}
+
+		return bestMatch, nil
 	}
 
 	return filtered[0], nil
+}
+
+type ambiguousError string
+
+func (a ambiguousError) Error() string {
+	return fmt.Sprintf("term %q is ambiguous", string(a))
+}
+
+type matchType int
+
+const (
+	noMatch matchType = iota
+	exactMatch
+	partialMatch
+)
+
+func matches[T Filterable](item T, term string) matchType {
+	identifiers := item.Keys()
+	for _, identifier := range identifiers {
+		identifier = strings.ToLower(identifier)
+
+		if strings.EqualFold(identifier, term) {
+			return exactMatch
+		}
+
+		if strings.Contains(identifier, term) {
+			return partialMatch
+		}
+	}
+
+	return noMatch
 }
